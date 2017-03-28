fb6a2f5c8c667d2452dea35e38fa2161a908703a
Phil <Philip.Shaughnessy@gmail.com>
2017-03-28 09:16:30 -0400

    namespaces routes under api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :products
-  resources :users
+  scope :api do
+    resources :orders
+    resources :products
+    resources :users
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
