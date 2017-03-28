5973b25d29070569940f189dd17c8a14a8252cb7
Gabe Wright <gabe1331@gmail.com>
2017-03-28 17:13:40 -0400

    start ajax testing

--- app-react/public/js/global.js
+++ app-react/public/js/global.js
@@ -1,3 +1,22 @@
 // homepage
 
-console.log('sup');
+getUsers();
+
+// First AJAX request that gets users
+function getUsers() {
+        // Fetch the raw user JSON string data
+        // var token = sessionStorage.getItem('token');
+        fetch('')
+            .then(response => response.json())
+            .then(data => {
+                products = data
+                loopUsers(products);
+            });
+    }
+
+    function loopUsers(products) {
+        products.forEach(createProducts);
+        console.log(createProducts)
+}
+    // document.querySelector('#userListInput').innerHTML += userList;
+
