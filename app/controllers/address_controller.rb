30584eccf9af6882313ab09c3de8fa0f2d21f1df
Phil <Philip.Shaughnessy@gmail.com>
2017-03-28 20:18:36 -0400

    added some controller methods, address controller, user_extended_serializer

---
+++ app/controllers/address_controller.rb
@@ -0,0 +1,28 @@
+class AddressController < ApplicationController
+
+  def index
+    @address = Address.all
+    render json: @order
+  end
+
+  def show
+    @address = Address.find(params[:id])
+    render json: @address
+  end
+
+  def create
+    @address = Address.new(address_params)
+
+    if @address.save
+      render json: @address
+    else
+      request_error(@address.errors.fufull_messages)
+    end
+  end
+
+  private
+
+  def address_params
+    params.permit(:name, :street, :city, :state, :zip)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,37 @@
 class UsersController < ApplicationController
-  
+
+  def index
+    @user = User.all
+    render json: @order
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, serializer: UserExtendedSerializer
+    else
+      request_error(@user.errors.full_messages)
+    end
+  end
+
+  def login
+    @user = User.find_by(username: params[:username])&.
+              authenticate(params[:password])
+    if @user
+      render json: @user, serializer: UserExtendedSerializer
+    else
+      request_error("Invalid username or password", 401)
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password)
+  end
+
 end
