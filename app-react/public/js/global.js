db340c080331bb815955432c79892ee76cd90049
Gabe Wright <gabe1331@gmail.com>
2017-03-29 09:03:43 -0400

    test 1 finished

--- app-react/public/js/global.js
+++ app-react/public/js/global.js
@@ -2,21 +2,20 @@
 
 getUsers();
 
-// First AJAX request that gets users
 function getUsers() {
-        // Fetch the raw user JSON string data
-        // var token = sessionStorage.getItem('token');
-        fetch('')
+        fetch('/api/products')
             .then(response => response.json())
             .then(data => {
                 products = data
-                loopUsers(products);
+                loopProducts(products);
             });
     }
 
-    function loopUsers(products) {
-        products.forEach(createProducts);
-        console.log(createProducts)
-}
+    function loopProducts(products) {
+        products.forEach(createProducts => {
+            console.log(createProducts)
+        }) 
+        
+    }
     // document.querySelector('#userListInput').innerHTML += userList;
 
