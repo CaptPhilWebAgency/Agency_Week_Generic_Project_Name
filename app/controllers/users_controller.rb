159631db09392921aad8d5b6c6a29e8d0dfbb3cb
CosmicCandi <kalea.wolff@gmail.com>
2017-03-30 16:40:09 -0400

    Updating Users Controller to prepare for stripe.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    @user = User.all
-    render json: @order
+    if current_user
+      @user = User.find(params[:id])
+      render json: @order
+    else
+      render json: @products
+    end
   end
 
   def show
@@ -13,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UsersMailer.signup(@user).deliver
       render json: @user, serializer: UserExtendedSerializer
     else
       request_error(@user.errors.full_messages)
