9a33ccceac5b2d2eb33da880ef331936c38b5dec
CosmicCandi <kalea.wolff@gmail.com>
2017-03-31 12:21:55 -0400

    Makes street2 and street3 optional fields.

--- db/migrate/20170331161854_add_street2_and3_to_addresses.rb
+++ db/migrate/20170331161854_add_street2_and3_to_addresses.rb
@@ -1,6 +1,6 @@
 class AddStreet2And3ToAddresses < ActiveRecord::Migration[5.0]
   def change
-    add_column :addresses, :street2, :string
-    add_column :addresses, :street3, :string
+    add_column :addresses, :street2, :string, required: false
+    add_column :addresses, :street3, :string, required: false
   end
 end
