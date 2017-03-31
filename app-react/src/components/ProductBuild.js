f8cc1c1961c3f6ffbe5bf721ee2e49caa5d9781f
Ragen Sanner <ragens9@att.net>
2017-03-31 11:54:08 -0400

    products columns now working, added add-to-cart button on products page

--- app-react/src/components/ProductBuild.js
+++ app-react/src/components/ProductBuild.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 
 class Product extends Component {
   render() {
-    return (<div className="column is-one-third-mobile is-one-third-tablet is-one-quarter-desktop is-offset-4 productCard">
+    return (<div className="column is-one-third productCard">
       <div className="section">
-          <img className="productImgBuild" src={this.props.productImg} />
+          <img className="productImgBuild" src={this.props.productImg} alt="patch" />
           <h4 className="productTitleBuild">{this.props.productTitle} &#8212; <span className="productPriceBuild">{this.props.productPrice}</span></h4>
+          <p className="control has-text-centered">
+            <button type="button" className="button cartButton">
+                Add to cart
+            </button>
+          </p>
       </div>
 
     </div>
--- app-react/src/components/Products.js
+++ app-react/src/components/Products.js
@@ -11,52 +11,39 @@ class Products extends Component {
     return (
       <div>
         <HeaderSub />
-        <h2 className="sectionHeadersSub">National Parks</h2>
-        <aside class="menu">
-          <div className="field has-addons">
-            <p className="control">
-                <input className="input" type="text" placeholder="Search products"/>
-            </p>
-            <p className="control">
-              <a className="button">
-                Search
-              </a>
-            </p>
+          <h2 className="sectionHeadersSub">Patches</h2>
+            <div className="productsSideMenu columns is-multiline is-mobile categories">
+              <div className="column is-one-quarter">
+                <div className="field has-addons">
+                  <p className="control">
+                    <input className="input" type="text" placeholder="Search products"/>
+                  </p>
+                  <p className="control">
+                    <a className="button">
+                      Search
+                    </a>
+                  </p>
+                </div>
+
+                <br/>
+                <h4>Sort By Category</h4>
+                <a className="categoryTitlesProductsPage" href="">all</a><br/>
+                <a className="categoryTitlesProductsPage" href="">music</a><br/>
+                <a className="categoryTitlesProductsPage" href="">national parks</a><br/>
+                <a className="categoryTitlesProductsPage" href="">farming</a><br/>
+                <a className="categoryTitlesProductsPage" href="">movies</a>
+              </div>
+            
+
+            <div className="column">
+              <div className="columns is-multiline is-mobile categories">
+                <ProductBuild productImg="./img/igp20.png" productTitle="Rocky Mountain" productPrice="$134" />
+                <ProductBuild productImg="./img/igp21.png" productTitle="BANFF Canada" productPrice="$84" />
+                <ProductBuild productImg="./img/igp22.png" productTitle="Mt Rainier" productPrice="$42" />
+                <ProductBuild productImg="./img/igp23.png" productTitle="Yellowstone" productPrice="$24" />   
+              </div>
           </div>
-          <p class="menu-label">
-            Categories
-          </p>
-          <ul class="menu-list">
-            <li><a>all</a></li>
-            <li><a>music</a></li>
-            <li><a>national parks</a></li>
-            <li><a>farming</a></li>
-            <li><a>movies</a></li>
-          </ul>
-        </aside>
-
-
-
-
-
-          <div className="columns is-multiline is-mobile">
-            <div className="column is-one-third-mobile is-one-third-tablet is-one-quarter-desktop is-offset-1">
-              
-
-              <a className="categoryTitlesProductsPage" href="">all</a><br/>
-              <a className="categoryTitlesProductsPage" href="">music</a><br/>
-              <a className="categoryTitlesProductsPage" href="">national parks</a><br/>
-              <a className="categoryTitlesProductsPage" href="">farming</a><br/>
-              <a className="categoryTitlesProductsPage" href="">movies</a>
-            </div>
-
-            <div className="productsList">
-              <ProductBuild productImg="./img/igp20.png" productTitle="Rocky Mountain" productPrice="$134" />
-              <ProductBuild productImg="./img/igp21.png" productTitle="BANFF Canada" productPrice="$84" />
-              <ProductBuild productImg="./img/igp22.png" productTitle="Mt Rainier" productPrice="$42" />
-              <ProductBuild productImg="./img/igp23.png" productTitle="Yellowstone" productPrice="$24" />
-            </div>
-          </div>
+        </div>
 
           <Footer />
       </div>
